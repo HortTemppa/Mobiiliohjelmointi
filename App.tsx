ce6170149835c07b421da0d62ecda71d2e190649
Teemu Laine <teemu.laine@myy.haaga-helia.fi>
2022-08-24 12:53:56 +0300

    Laskimen rakenne tehty

--- App.tsx
+++ App.tsx
@@ -1,12 +1,43 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, TextInput, View, Button } from "react-native";
 
 export default function App() {
+  const [result, setResult] = useState();
+
+  const [firstNumber, setFirstNumber] = useState(0);
+  const [secondNumber, setSecondNumber] = useState(0);
+
+  const addNumbers = (): void => {
+    setResult(firstNumber + secondNumber);
+  };
+
+  const subtractNumbers = (): void => {
+    setResult(firstNumber - secondNumber);
+  };
+
+  const handleFirstChange = (e): void => {
+    setFirstNumber(e.target.value);
+  };
+
+  const handleSecondChange = (e): void => {
+    setSecondNumber(e.target.value);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Tähän tulee appi</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        keyboardType="numeric"
+        onChange={handleFirstChange}
+        value={firstNumber}
+      ></TextInput>
+      <TextInput
+        keyboardType="numeric"
+        onChange={handleSecondChange}
+        value={secondNumber}
+      ></TextInput>
+      <Button onPress={addNumbers} title="+"></Button>
+      <Button onPress={subtractNumbers} title="-"></Button>
     </View>
   );
 }
